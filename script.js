6769a5198c21c46cd4ada9486322d90fd8b96841
Mateus Freire <mateusfreire.dev@gmail.com>
2024-10-17 10:41:18 +0100

    Função para começar o Jogo

---
+++ script.js
@@ -0,0 +1,20 @@
+const btnstart = document.getElementById("btnstart")
+const section2 = document.getElementById("section2")
+const h2 = document.getElementById("h2")
+
+btnstart.addEventListener("click", function(ev){
+    ev.preventDefault()
+
+    section2.style.display = "block"
+
+    const player1 = document.getElementById("player1").value
+    const player2 = document.getElementById("player2").value
+
+    h2.innerText = "Jogador " + player1 + " é a sua vez!"
+
+    executeGame()
+})
+
+function executeGame (){
+    
+}
